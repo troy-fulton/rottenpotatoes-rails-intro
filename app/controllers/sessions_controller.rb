47e0dbe41817985513e7756184d3fb9e8880f2fb
Troy Fulton <troyfulton@tamu.edu>
2020-03-17 19:36:46 +0000

    Fixed ActiveRecord#where() problem in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,10 @@ class SessionsController < ApplicationController
     skip_before_filter :set_current_user
     def create
         auth = request.env["omniauth.auth"]
-        user = Moviegoer.where(:provider => auth["provider"], 
-                               :uid => auth["uid"]) ||
-            Moviegoer.create_with_omniauth(auth)
+        user = Moviegoer.find_by(:provider => auth["provider"], :uid => auth["uid"])
+        if user == nil
+            user = Moviegoer.create_with_omniauth(auth)
+        end
         session[:user_id] = user.id
         flash[:notice] = 'Logged #{@current_user.name} in successfully'
         redirect_to movies_path
