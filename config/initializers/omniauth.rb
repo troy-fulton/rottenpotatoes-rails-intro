7d2cd8443ac9533750844364b51ec7335af4d31c
Troy Fulton <troyfulton@tamu.edu>
2020-03-17 19:13:04 +0000

    Added API key and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "API_KEY", "API_SECRET" 
+  provider :twitter, "fU63yCZVH1hSHJG4nAKGkRgiY", "eOJdZ7Ktfxv2BLOkDPd3rRhJfKCBF6Sy1ott1q4ge3EdJrV1ff" 
 end
