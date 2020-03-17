368ec68e0f5d684c703a3bd1596381e8596bb436
Troy Fulton <troyfulton@tamu.edu>
2020-03-17 19:41:37 +0000

    Fixed a possible naming error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
     skip_before_filter :set_current_user
     def create
         auth = request.env["omniauth.auth"]
-        user = Moviegoer.find_by(:provider => auth["provider"], :uid => auth["uid"])
+        @current_user = Moviegoer.find_by(:provider => auth["provider"], :uid => auth["uid"])
         if user == nil
-            user = Moviegoer.create_with_omniauth(auth)
+            @current_user = Moviegoer.create_with_omniauth(auth)
         end
         session[:user_id] = user.id
         flash[:notice] = 'Logged ' + @current_user.name.to_s + ' in successfully'
